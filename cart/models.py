fb302cb14aa82b8ea1ef606d3f4169bab27147c8
GoshaCatharsis <kremenevskiy97@mail.ru>
2022-12-19 19:03:51 +0300

    lesson 26

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Cart(models.Model):
-    product = models.ForeignKey('catalog.Book', on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField()
-    price = models.FloatField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    product = models.ManyToManyField('catalog.Book', blank=True)
+
+    def get_total_price(self):
+        return round(sum([book.price for book in self.product.all()]), 2)
+
+    def get_total_quantity(self):
+        return len(([book for book in self.product.all()]))
+
+    def display_products(self):
+        return ', '.join([book.title for book in self.product.all()])
 
 
